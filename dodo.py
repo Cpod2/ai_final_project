d200879a61f646e66ccab79bb4a93b9fcb980388
Nicolas Oliver <nicolasoliver03@gmail.com>
2023-03-21 05:05:54 +0000

    Update notebook
    * Add nbqa for automatic formatting
    * Add some details about project

--- dodo.py
+++ dodo.py
@@ -9,10 +9,14 @@ def task_fixup():
             "pyupgrade --py311-plus",
             "black .",
             "mdformat .",
+            "nbqa isort --profile=black index.ipynb",
+            "nbqa pyupgrade --py311-plus index.ipynb",
+            "nbqa black index.ipynb",
+            "nbqa --nbqa-md mdformat --wrap=100 --number index.ipynb",
         ]
     }
 
 
 def task_test():
     """Run Tests"""
-    return {"actions": ["pylint --recursive=y ."]}
+    return {"actions": ["pylint --recursive=y .", "nbqa pylint index.ipynb"]}
